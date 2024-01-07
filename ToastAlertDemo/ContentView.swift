3e61fe148ed2f33598304f6576b55fdbf9b0442a
Terry Kuo <terrykuo501@gmail.com>
2024-01-07 18:57:52 +0800

    Initial Commit

---
+++ ToastAlertDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToastAlertDemo
+//
+//  Created by Terry Kuo on 2024/1/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToastAlertDemo/ToastAlertDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastAlertDemoApp.swift
+//  ToastAlertDemo
+//
+//  Created by Terry Kuo on 2024/1/7.
+//
+
+import SwiftUI
+
+@main
+struct ToastAlertDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
